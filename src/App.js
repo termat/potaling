6501a815e09f00455d0ca4984f10d378448ec1b8
termat <ter.matsuoka@gmail.com>
2021-05-19 14:45:25 +0900

    微修正

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import Dashboard from './components/Dashboard';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>ポタリングの記録（実験版）</title>
+      </Helmet>
+      <Dashboard />
+    </HelmetProvider>
   );
 }
 
