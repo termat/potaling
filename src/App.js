c0771e33bb1939591f75db39605b2b3b56b384e3
termat <ter.matsuoka@gmail.com>
2021-05-22 10:26:14 +0900

    微修正

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
     <HelmetProvider>
       <Helmet>
         <title>ポタリングの記録（実験版）</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@t_mat" />
+        <meta property="og:url" content="https://termat.github.io/potaling/" />
+        <meta property="og:title" content="ポタリングの記録（実験版）" />
+        <meta property="og:description" content="ポタリングした地域を3D地図上で俯瞰するWebアプリです。" /> 
+        <meta property="og:image" content={`${window.location.origin}/images/back.jpg`} />
       </Helmet>
       <Dashboard />
     </HelmetProvider>
