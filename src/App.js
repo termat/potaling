d02cc77ce7e5f6aea4eb3725de4edae254a39b26
termat <ter.matsuoka@gmail.com>
2021-05-22 11:25:03 +0900

    微修正

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <HelmetProvider>
-      <Helmet>
-        <title>ポタリングの記録（実験版）</title>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@t_mat" />
-        <meta property="og:url" content="https://termat.github.io/potaling/" />
-        <meta property="og:title" content="ポタリングの記録（実験版）" />
-        <meta property="og:description" content="ポタリングした地域を3D地図上で俯瞰するWebアプリです。" /> 
-        <meta property="og:image" content={`${window.location.origin}/images/back.jpg`} />
-      </Helmet>
       <Dashboard />
-    </HelmetProvider>
   );
 }
 
